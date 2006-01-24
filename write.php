b92c151c2b3b85d541731a576cda1be4b261c435
mpapis <mpapis>
2006-01-24 01:02:50 +0000

    corrected IE behaviour, now on IE and on other brovsers we have following behavior:
    1) on loda focus set to write window (edit)
    2) after presing Enter (post) edit window is cleared

--- write.php
+++ write.php
@@ -4,47 +4,46 @@
 	<title>ShoutBox</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-2">
 </head>
-<body><?php
-$useragent = getenv("HTTP_USER_AGENT");
-
-if(isset($_REQUEST['name']))
+<?php
+if (preg_match("/MSIE/i", "$useragent"))
 {
-  if (preg_match("/MSIE/i", "$useragent"))
-  {
-    echo '		<form name="writeform" action="mesage.php?name=' . $_REQUEST['name'] . '" method="post" target="msg" style="border: none" >';
-  }
-  else
-  {
-    echo '		<form name="writeform" action="mesage.php?name=' . $_REQUEST['name'] . '" method="post" target="msg" style="border: none" onsubmit="document.writeform.body.value=document.writeform.body_.value; document.writeform.body_.value=\'\';">';
-  }
-  echo '			<input type="hidden" name="name" value="' . $_REQUEST['name'] . '" id="name" />';
+echo '<body onload="document.all.body_.focus();">
+';
 }
 else
 {
-  if (preg_match("/MSIE/i", "$useragent"))
-  {
-    echo '		<form name="writeform" action="mesage.php" method="post" target="msg" style="border: none" >';
-  }
-  else
-  {
-    echo '		<form name="writeform" action="mesage.php" method="post" target="msg" style="border: none" onsubmit="document.writeform.body.value=document.writeform.body_.value; document.writeform.body_.value=\'\';">';
-  }
-  echo '			User:<input type="text" name="name" value="" size="23" id="name" onclick="if (this.value==\'Name\') this.value = \'\';" />';
+echo '<body onload="document.writeform.body_.focus();">
+';
 }
+$useragent = getenv("HTTP_USER_AGENT");
 
 if (preg_match("/MSIE/i", "$useragent"))
 {
-echo '			<input type="text" name="body" value="" size="50" maxsize="200" style="width:100%" id="body" onclick="if (this.value==\'Message\') this.value = \'\';" />
-			<button accesskey="C" name="clear" type=reset><ins>C</ins>zysc</button>
-			<input type="hidden" name="shout" />';
+  $post_script = 'document.all.body.value=document.all.body_.value; document.all.body_.value=\'\';';
 }
 else
 {
-echo '			<input type="text" name="body_" value="" size="50" maxsize="200" style="width:100%" id="body_" onclick="if (this.value==\'Message\') this.value = \'\';" />
-			<input type="hidden" name="body" value="" id="body" readonly="false" />
-			<input type="hidden" name="shout" />';
+  $post_script = 'document.writeform.body.value=document.writeform.body_.value; document.writeform.body_.value=\'\';';
 }
-echo '</form>';
+
+if(isset($_REQUEST['name']))
+{
+echo '<form name="writeform" action="mesage.php?name=' . $_REQUEST['name'] . '" method="post" target="msg" style="border: none" onsubmit="' . $post_script . '">
+';
+echo '  <input type="hidden" name="name" value="' . $_REQUEST['name'] . '" id="name" />
+';
+}
+else
+{
+echo '<form name="writeform" action="mesage.php" method="post" target="msg" style="border: none" onsubmit="' . $post_script . '">
+';
+echo '  User:<input type="text" name="name" value="" size="23" id="name" onclick="if (this.value==\'Name\') this.value = \'\';" />
+';
+}
 ?>
+  <input type="text" name="body_" value="" size="50" maxsize="200" style="width:100%" id="body_" onclick="if (this.value=='Message') this.value = '';" />
+  <input type="hidden" name="body" value="" id="body" readonly="false" />
+  <input type="hidden" name="shout" />
+</form>
 </body>
 </html>
